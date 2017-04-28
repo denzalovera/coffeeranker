114a828f3593d377b934585610908da0d1800348
denzalovera <denz.alovera@gmail.com>
2017-04-28 19:26:07 +0800

    setup environment configurations.

--- config/config.py
+++ config/config.py
@@ -3,7 +3,34 @@ Global configurations goes here.
 created by: denz 4/27/2017
 '''
 
-# create logic that will turn to false when in different environment.
-# for now this works.
+from utility import key_gen
 
-DEBUG = True
+class Config(object):
+    """
+    common configuration
+    """
+    # TODO: think of global configs used across all environments to be placed here. Best Practice etc.
+
+
+class DevelopmentConfig(Config):
+    """
+    Development Environment
+    """
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+
+class ProductionConfig(Config):
+    """
+    Production Environment
+    """
+    DEBUG = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
+
+SECRET_KEY = key_gen.generate_password(8)
+# TODO need to setup scotch as mysql server and figure out how to install mysql-python
+SQL_ALCHEMY_DATABASE_URI = ''
