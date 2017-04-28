02fd18d7e5aa523c87b0e18ded7141a1ea80c43a
denzalovera <denz.alovera@gmail.com>
2017-04-28 19:35:03 +0800

    implement environment configuration to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,20 @@
 # the app will start to run instances of this script
 
 from flask import Flask
-'''
+from flask_sqlalchemy import SQLAlchemy
+
+"""
 created by: denz 04/27/2017
-'''
-app = Flask(__name__, instance_relative_config=True)
+"""
+from config.config import app_config
 
-# Load views
-from app import views
+db = SQLAchemy()
 
-# Load config files
-app.config.from_object('config.config')
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    # Load config files
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    return app
--- coffeeranker.py
+++ coffeeranker.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from app import app
-
+from app import create_app
+import os
 '''
 created by: denz 04/27/2017
 '''
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
+
 if __name__ == '__main__':
     app.run()
