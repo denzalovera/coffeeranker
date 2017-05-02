ec51e456edfd065d2f670574a2dbf652fb10b9ed
denzalovera <denz.alovera@gmail.com>
2017-05-02 12:02:33 +0800

    temporarily disable placeholders

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 # all app routes goes here.
 from flask import render_template
-from app import app
+# from app import app
 
 '''
 created by: denz 04/27/2017
 '''
-
+'''
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -14,3 +14,5 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+'''
