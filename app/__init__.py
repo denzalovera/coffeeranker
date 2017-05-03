651c7e47845763345a9b68e1be625c6049c1a05f
denzalovera <denz.alovera@gmail.com>
2017-05-03 11:05:11 +0800

    create database model

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 # third-party imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 """
 created by: denz 04/27/2017
@@ -13,7 +14,7 @@ from config import app_config
 
 # db variable initialization
 db = SQLAlchemy()
-
+login_manager = LoginManager()
 
 def create_app(config_name):
 
@@ -23,9 +24,10 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    # temporary route
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    # login manager
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
+
 
     return app
