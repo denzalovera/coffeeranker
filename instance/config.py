078e3a49b4ec5731920f3b211e731557ad74c931
denzalovera <denz.alovera@gmail.com>
2017-05-04 11:03:28 +0800

    modify db credentials

--- instance/config.py
+++ instance/config.py
@@ -2,6 +2,7 @@ from coffee_utils import keygen
 
 # create a random keygen of length 8
 SECRET_KEY = keygen.generate_password(8)
+
 # connect to vagrant mysql database address: 192.168.33.10/24
-# format: 'db_type://user:password@address/database'
-SQL_ALCHEMY_DATABASE_URI = 'mysql://cr_admin:admin@192.168.33.10/coffeeranker'
+# format: 'mysql://username:password@IP_ADDR:PORT/db_name'
+SQLALCHEMY_DATABASE_URI = 'mysql://cr_admin:admin@192.168.33.10:3306/coffeeranker'
