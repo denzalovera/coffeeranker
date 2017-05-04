b5e729e9f0667ef439e5915e75956dc49b9fe01c
denzalovera <denz.alovera@gmail.com>
2017-05-04 11:52:06 +0800

    remove .idea and instance dir, copy requirements.txt to scotch for virtualenv setup

--- instance/config.py
+++
@@ -1,8 +0,0 @@
-from coffee_utils import keygen
-
-# create a random keygen of length 8
-SECRET_KEY = keygen.generate_password(8)
-
-# connect to vagrant mysql database address: 192.168.33.10/24
-# format: 'mysql://username:password@IP_ADDR:PORT/db_name'
-SQLALCHEMY_DATABASE_URI = 'mysql://cr_admin:admin@192.168.33.10:3306/coffeeranker'
