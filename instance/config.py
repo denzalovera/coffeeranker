df49ea46097d705ef0cdc33aa64011dbee45ee9e
denzalovera <denz.alovera@gmail.com>
2017-05-08 14:16:01 +0800

    db instance and ingore file update

---
+++ instance/config.py
@@ -0,0 +1,10 @@
+from coffee_utils import keygen
+SECRET_KEY = keygen(8)
+
+DB_ENGINE = 'mysql'
+DB_USER = 'cr_admin'
+DB_PASSWORD = 'admin'
+IP_ADDR = '192.168.33.10'
+DB = 'coffeeranker'
+
+SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}/dreamteam_db'.format(DB_ENGINE, DB_USER, DB_PASSWORD, IP_ADDR, DB)
