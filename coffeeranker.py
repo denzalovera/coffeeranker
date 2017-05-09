aeabd626c791acd78a90adce9866392bbe4d25f6
denzalovera <denz.alovera@gmail.com>
2017-05-09 18:13:24 +0800

    remove TODO comments

--- coffeeranker.py
+++ coffeeranker.py
@@ -9,7 +9,3 @@ app = create_app(config_name)
 
 if __name__ == '__main__':
     app.run()
-
-# TODO: can't run db_migration missing library 'mysql-python'
-# we need to move to a Linux virtualenv for project,
-# easiest way to install this is through a linux box, have to setup scotch as new virtualenv.
