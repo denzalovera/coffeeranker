8b0a4164de63d95fc2ea202fc028495f3d3a3465
denzalovera <denz.alovera@gmail.com>
2017-06-09 17:21:09 -0400

    fix department module

--- coffeeranker.py
+++ coffeeranker.py
@@ -7,6 +7,7 @@ created by: denz 04/27/2017
 config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
+#TODO: fix department module backend and frontend
 
 if __name__ == '__main__':
     app.run()
